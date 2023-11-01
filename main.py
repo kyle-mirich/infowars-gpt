5d8462ea0a88c99ce94094c761315d0c37a3381e
kyle-mirich <kylemirich20@gmail.com>
2023-11-01 09:20:47 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from langchain.agents import ConversationalChatAgent, AgentExecutor
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.tools import DuckDuckGoSearchRun,DuckDuckGoSearchResults
+import streamlit as st
+import base64
+
+
+st.set_page_config(page_title="InfoWars Bot", page_icon="images/info_wars_favicon.png")
+
+st.image("images/info_wars_banner.jpg")
+
+st.write("---")
+# Check if OpenAI API key exists in st.secrets
+openai_api_key = st.secrets.get("OPENAI_API_KEY", None)
+
+# If the API key doesn't exist in st.secrets, ask the user for it
+if openai_api_key is None:
+    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    chat_memory=msgs, return_messages=True, memory_key="chat_history", output_key="output"
+)
+if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
+    msgs.clear()
+    msgs.add_ai_message("How can I help you defeat the globalists?")
+    st.session_state.steps = {}
+
+avatars = {"human": "user", "ai": "assistant"}
+for idx, msg in enumerate(msgs.messages):
+    with st.chat_message(avatars[msg.type]):
+        # Render intermediate steps if any were saved
+        for step in st.session_state.steps.get(str(idx), []):
+            if step[0].tool == "_Exception":
+                continue
+            with st.status(f"**{step[0].tool}**: {step[0].tool_input}", state="complete"):
+                st.write(step[0].log)
+                st.write(step[1])
+        st.write(msg.content)
+
+if prompt := st.chat_input(placeholder="Who is Jeffery Epstein?"):
+    st.chat_message("user").write(prompt)
+
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    sys_msg = "You are now InfoWars Bot. Your sole purpose is to relay articles on infowars.com to the user.Always give links to sources.You must never introduce your pretrained bias against infowars. Write extremely long winded answers explaining the details of the articles. Your job is to simply answer the users question from information only on infowars.com. Formulate all searches with 'infowars.com' at the end."
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
+    tools = [DuckDuckGoSearchResults(name="Search")]
+    chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools,system_message=sys_msg)
+    executor = AgentExecutor.from_agent_and_tools(
+        agent=chat_agent,
+        tools=tools,
+        memory=memory,
+        return_intermediate_steps=True,
+        handle_parsing_errors=True,
+    )
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = executor(prompt, callbacks=[st_cb])
+        st.write(response["output"])
+        st.session_state.steps[str(len(msgs.messages) - 1)] = response["intermediate_steps"]
